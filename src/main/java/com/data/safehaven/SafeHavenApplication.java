b138a285dc27daaaafbb21718ee0649b04f72d20
anuarthr <anuarthrincongonzalez@gmail.com>
2024-10-23 11:54:24 -0500

    create spring application

---
+++ src/main/java/com/data/safehaven/SafeHavenApplication.java
@@ -0,0 +1,13 @@
+package com.data.safehaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeHavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeHavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/data/safehaven/SafeHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.safehaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeHavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
